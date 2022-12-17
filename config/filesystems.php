3a4c77119df44cc6f4cbee0f3c30db45b33f0df2
Digital <widayatmurni@gmail.com>
2022-12-18 06:24:14 +0900

    reinit

--- config/filesystems.php
+++ config/filesystems.php
@@ -71,8 +71,8 @@ return [
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
-        // public_path('assets') => storage_path('app/public/assets'),
-        // public_path('uploads') => storage_path('app/public/uploads'),
+        public_path('assets') => storage_path('app/assets'),
+        public_path('uploads') => storage_path('app/uploads'),
     ],
 
 ];
